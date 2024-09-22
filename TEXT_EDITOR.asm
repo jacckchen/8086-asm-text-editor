f7711728aec1595372544830a20a1e68158394c2
jacckchen <129080425+jacckchen@users.noreply.github.com>
2024-09-22 19:24:27 +0800

    my own text_editor for the course

--- TEXT_EDITOR.asm
+++ TEXT_EDITOR.asm
@@ -1,182 +1,175 @@
-.MODEL SMALL
-.DATA
-    matrix      db 80*25 dup(?),'$' ;25 lines of 80 chars each 
-    matrix_2    db 22 dup(?)        ;   
-    row         db 2                ;for navigating columns/character cells with arrow keys
-    column      db 0                ;for navigating rows/lines with arrow keys
-    curr_line   db 2                ;rows/lines while editing
-    curr_char   db 0                ;columns/chars while editing
-    ;for main menu
+DATAS SEGMENT
+    
+    filename db 255,?,255 dup('$')
+    fh dw ?
+    row db 0
+    col db 0
+    lines db 0	;储存文件行数
+    numbers db 255 dup(0)	;存储每行的字符数
+    deta dw 0
+    newline db 0dh,0ah,'$'
+    newfile db 'create newfile.$'
+    errorx db 'newfile error.$'
+    errord db 'openfile error.$'
+    errorinfo db 'open or create file error!The program will exit.$'
     deco1       db '  =================================================$'
     deco2       db '||            Command Line Text Editor             ||$'
-    deco3       db '||                                                 ||$'
+    deco3       db '||    it will create file if it doesn''t exist     ||$'
     deco4       db '||         ESC = Exit || CTRL+S = Save File        ||$'
     deco5       db '||              ARROW KEYS = Navigate              ||$'
     deco6       db '  =================================================$'
     docPrompt   db 'Enter Document Name (.txt): $'
-    docName     dw 50 dup(?),'$'
-    openPrompt  db 'Enter Document Name to Open: $'
-    HANDLE      dw ? 
-    header      db 80 dup('='),'$'    
-    color       db 3*15+15
-    
-          
-.CODE 
+	buffer db 2000 dup(0)
+    ;此处输入数据段代码  
+DATAS ENDS
 
-;=========== MACROS ===========
-newline macro
-    mov dl, 10       ;newline ASCII
-    mov ah, 2
-    int 21h   
-    mov dl, 13       ;linefeed (return) ASCII
-    mov ah, 2
+STACKS SEGMENT
+    ;此处输入堆栈段代码
+STACKS ENDS
+;dh=row,dl=col,di=offset
+Video_addr macro width,page_num
+	push ax
+	push dx
+	mov al,width
+	mul dh
+	xor dh,dh
+	add ax,dx
+	shl ax,1
+	add ax,page_num*100h
+	mov di,ax
+	pop dx
+	pop ax
+	endm
+CODES SEGMENT
+    ASSUME CS:CODES,DS:DATAS,SS:STACKS
+START: 
+main proc far
+    MOV AX,DATAS
+    MOV DS,AX
+    
+    
+    mov ah,5
+    mov row,ah
+    mov al,12
+    mov col,al
+    call far ptr goto_pos 
+    lea dx,deco1      ;decoration 1
+    mov ah, 9
     int 21h
-endm
-remove macro
-    mov dx, 8        ;backspace to go back one char
-    mov ah, 2
+    inc row
+    call far ptr goto_pos
+    lea dx,deco2      ;decoration 2
     int 21h
-    mov dx, 32       ;space to remove that char
-    mov ah, 2
+    inc row
+    call far ptr goto_pos
+    lea dx,deco3      ;decoration 3
     int 21h
-    mov dx, 8        ;backspace to go back at removed char position
-    mov ah, 2
+    inc row
+    call far ptr goto_pos
+    lea dx,deco4      ;decoration 4
     int 21h
-endm
-goto_pos macro row, col
-    mov ah, 02h      ;set text position in middle screen
-    mov dh, row
-    mov dl, col
-    int 10h
-endm
-clrScrn macro
-    mov ah, 02h    ;set cursor to upper left corner
-    mov dh, 0
-    mov dl, 0
-    int 10h            
-    mov ah, 0Ah    ;overwrite with blank chars & remove all chars
-    mov al, 00h    ;character
-    mov cx, 2000  ;how many times to write
-    int 10h        ;graphics interrupt
-endm 
-debug macro arg
-    mov dx, arg    ;for debugging purpose
-    mov ah, 2
+    inc row
+    call far ptr goto_pos
+    lea dx,deco5      ;decoration 5
     int 21h
-endm
+   	inc row
+    call far ptr goto_pos
+    lea dx,deco6      ;decoration 6
+    int 21h
+    mov al,13
+    mov row,al
+    call far ptr goto_pos
+    lea dx,docPrompt  ;prompt doc name field
+    int 21h
+    
+    ;读取文件路径
+    lea dx,filename
+    mov ah,0ah
+    int 21h
+    
+    ;修正0dh的错误
+    mov al,0
+    mov bl,[filename+1]
+    mov bh,0
+    mov filename[bx+2],al
+    
+    ;尝试打开文件
+    lea dx,filename[2]
+    mov ah,3dh
+    mov al,2
+    int 21h
+    ;如果没出现错误
+    jnc next
+    
+    ;尝试新建文件
+    mov ah,3ch
+    mov cx,0
+    int 21h
+    jc error
+    ;清除屏幕
+    
+    mov ah,3dh
+    mov al,2
+    int 21h
+    ;jnc next2
+next:
+	;读取文件内容到缓冲区
+	mov fh,ax
+    mov bx,fh
+    lea dx,buffer
+    mov cx,1024
+    mov ah,3fh
+    int 21h
+    jc error
+    
+    
+    ;将文件内容输出到显存内
+    mov ax,0b800h
+    ;坐标数据清0
+    mov row,al
+    mov col,al
+    mov bx,0
+    mov es,ax
+    mov di,0000h
+    lea si,buffer
+    call far ptr clearscreen
+output:
+	mov dl,byte ptr ds:[si]
+	cmp dl,0
+	jz endoutput
+	
+	;遇到回车的处理
+	cmp dl,13d
+	je CR
+	
+	mov byte ptr es:[di],dl
+	inc si
+	inc numbers[bx]
+	add di,2
+jmp output
 
-;=============== PROCEDURES ===============
-start_menu proc
-    ;DISPLAY MAIN MENU
-    goto_pos 5, 12
-    mov dx, offset deco1      ;decoration 1
-    mov ah, 9
-    int 21h
-    goto_pos 6, 12
-    mov dx, offset deco2      ;decoration 2
-    mov ah, 9
-    int 21h
-    goto_pos 7, 12
-    mov dx, offset deco3      ;decoration 3
-    mov ah, 9
-    int 21h
-    goto_pos 8, 12
-    mov dx, offset deco4      ;decoration 4
-    mov ah, 9
-    int 21h
-    goto_pos 9, 12
-    mov dx, offset deco5      ;decoration 5
-    mov ah, 9
-    int 21h
-    goto_pos 10, 12
-    mov dx, offset deco6      ;decoration 6
-    mov ah, 9
-    int 21h
-    goto_pos 13, 12
-    mov dx, offset docPrompt  ;prompt doc name field
-    mov ah, 9
-    int 21h
-    
-    ;INPUT CHARS IN DOC NAME FIELD 
-    mov cx, 0  ;array size counter
-    mov si, offset docName
-    input_char: 
-    mov ah, 1
-    int 21h
-    cmp al, 13          ;check if return key hit
-    je return
-    cmp al, 8           ;check if backspace key hit
-    je remove_char
-    inc cx              ;increment array size by 1
-    mov [si], al
-    inc si
-    jmp input_char
-    
-    remove_char:
-    cmp cx, 0
-    je setPos_ret
-    dec cx              ;decrement array size by 1
-    dec si
-    mov [si], 00h
-    
-    mov dl, 32          ;for removing char
-    mov ah, 2           ;
-    int 21h             ;
-    mov dl, 8           ;
-    mov ah, 2           ;
-    int 21h             ;
-    jmp input_char
-    
-    setPos_ret:
-    goto_pos 13, 40
-    jmp input_char 
-    
-    return:    ;clear the screen and return procedure
-    ret 
-start_menu endp
-
-upper_bar proc
-    goto_pos 0 0
-    mov dx, offset docName  ;display DOCNAME on upper corner
-    mov ah, 9
-    int 21h
-    goto_pos 1 0
-    mov dx, offset header
-    mov ah, 9
-    int 21h
-    
-    ret            
-upper_bar endp
-
-;=============== MAIN ===============
-MAIN PROC
-    mov ax, @DATA
-    mov ds, ax 
-    
-    mov ah, 01h        ;Define Text Cursor Shape
-    mov cx, 07h        ;
-    int 10h            ; 
-    clrScrn
-    call start_menu    ;call start menu 
-    clrScrn            ;clear screen macro
-    call upper_bar     ;call upper stats bar in editor UI
-    
-    goto_pos 2, 0      ;set cursor position beneath upper bar
-    
-    mov si, offset matrix 
-    mov di, offset matrix_2
-    MAIN_LOOP:                                   
-    ; Get keystroke
-    mov ah, 00h
-    int 16h
-    ; AH = BIOS scan code
-    cmp ah, 01h            ;if escape key
-    je EXIT
-    cmp al, 13h            ;if CTRL+S
-    je SAVE
-    cmp al, 0Fh            ;if CTRL+O
-    je OPEN
-    cmp ah, 48h            ;if up arrow
+CR:
+	add si,2
+	inc lines
+	mov bl,lines
+	inc row
+	mov dl,0
+	mov dh,row
+	Video_addr 80,0
+jmp output
+endoutput:
+	
+	;初始化光标位置
+	mov bh,0
+	mov dx,0
+	mov ah,2
+	int 10h
+	mov row,bh
+	mov col,bh
+MAIN_LOOP:
+	mov ah,0
+	int 16h
+	cmp ah, 48h            ;if up arrow
     je UP
     cmp ah, 50h            ;if down arrow
     je DOWN
@@ -184,169 +177,461 @@ MAIN PROC
     je LEFT
     cmp ah, 4Dh            ;if right arrow
     je RIGHT                             
-    cmp ah, 1Ch            ;if enter (newline) key
-    je ENTER                                    
-    cmp ah, 0Eh            ;if backspace (remove character)
-    je BACKSPACE       
+	cmp ah,01h				;esc
+	je exit
+	cmp ah,0eh
+	je delchar
+	cmp ah, 1ch            ;if enter (newline) key
+    je toENTER
     
-    cmp column, 79
-    je ENTER
-    mov dl, al             ;if any other key then write char on screen
-    mov ah, 2
-    int 21h        
-    mov [si], al           ;add char in matrix array
-    inc si
-    inc curr_char          ;increment char position on current row
-    inc column             ;also increment the current character count
-    goto_pos row, column
-    jmp MAIN_LOOP
-         
-    EXIT:
-    mov ah, 4ch
-    int 21h
-        
-    SAVE:
-    mov ah, 3Ch             ;creating a file
-    mov cx, 0               ;read-only file
-    mov dx, offset docName  ;giving name which we took from Main Menu Doc Name Input
-    int 21h                 
-    mov ah, 3Dh             ;opening file
-    mov al, 1               ;for writing mode
-    mov dx, offset docName  ;which file
-    int 21h
-    mov HANDLE, ax          ;setting up handler
-    mov ah, 40h             ;function for writing files
-    mov bx, HANDLE          ;search for file handler
-    mov cx, 2000            ;how many bytes to write in file
-    mov dx, offset matrix   ;what to write
-    int 21h
-    jmp MAIN_LOOP  
+	;看来，dos在发现键盘按下ctrl键时，并不会执行完16h的0号功能
+	;而是与组合键一起发出一个设备控制字符后才接收该控制字符
+	;cmp ah,1dh 
+	;jne noctrl
+	
+	;mov ah,0
+	;int 16h
+	cmp al,19d
+	je toSAVE
+	
+	
+	push ax
+	mov al,80
+	mov dl,row
+	mul dl
+	shl ax,1
+	mov cl,col
+	mov ch,0
+	shl cx,1
+	add ax,cx
+	mov di,ax
+	pop ax
+	mov ch,col
+	mov bh,0
+	mov bl,row
+	;di->ah
+	;al->di
+	;ah->al
+movback:
+	mov ah,es:[di]
+	mov es:[di],al
+	;cmp ah,32d
+	;je next5
+	mov cl,8
+	shr ax,cl
+	add di,2
+	inc ch
+	cmp ch,numbers[bx]
+	ja next5
+jmp movback
+next5:
+	inc col
+	;mov bh,0
+	;mov bl,row
+	inc numbers[bx]
+	inc deta
+	call far ptr goto_pos
+jmp MAIN_LOOP
+	
+toENTER:
+	inc lines
+	mov dl,lines
+	mov bl,row
+	mov bh,0
+	sub dl,bl
+	inc bl
+	mov al,numbers[bx]
+	mov cl,8
+	
+incline:
+	mov ah,numbers[bx+1]
+	mov numbers[bx+1],al
+	shr ax,cl
+	inc bl
+	dec dl
+	jz nextinc
+jmp incline
+;将一行分为两行
+nextinc:
+	mov bh,0
+	mov bl,row
+	mov cl,col
+ 	mov dl,numbers[bx]
+ 	sub dl,cl
+ 	mov numbers[bx+1],dl
+ 	mov numbers[bx],cl
+ 	
+ 	
+ 	
+ 	;重新输出屏幕上的内容
+ 	mov cl,lines
+ 	mov dh,row
+ 	mov dl,0
+ 	sub cl,dh
+ 	mov ah,cl
+incline2:
+	inc dh
+	;Video_addr改变了dh的内容，这花费我一晚上去寻找这个bug，这警戒我子程序一定要保存恢复好寄存器
+	Video_addr 80,0
+ 	push di
+ 	dec cl
+ 	cmp cl,0ffh
+ 	je nono
+jmp incline2
+
+nono:
+	pop di
+incline3:	
+	pop si
+	push si
+	mov cx,79d
+copyline:
+	mov dl,es:[si]
+	mov es:[di],dl
+	add di,2
+	add si,2
+loop copyline
+	pop di
+	dec ah
+	cmp ah,0
+	je nextinc2
+jmp incline3
+;将一行分为两行
+nextinc2:
+	mov dh,row
+	mov dl,col
+	mov cx,80
+	sub cl,dl
+	Video_addr 80,0
+	mov si,di
+	inc dh
+	mov dl,0
+	Video_addr 80,0
+	mov dh,32d
+copy2:
+	mov dl,es:[si]
+	mov es:[di],dl
+	mov es:[si],dh
+	add di,2
+	add si,2
+	
+loop copy2
+ 	inc row
+ 	mov dl,0
+ 	mov col,dl
+ 	call far ptr goto_pos
+   jmp MAIN_LOOP
     
-    OPEN:
-    goto_pos 22 0    ;go to bottom to write open file prompt
-    mov dx, offset openPrompt
-    mov ah, 9
-    int 21h
-    ;INPUT CHARS IN DOC NAME FIELD 
-    mov cx, 0  ;array size counter
-    mov di, offset docName
-    input_char2: 
-    mov ah, 1
-    int 21h
-    cmp al, 13          ;check if return key hit
-    je return2
-    cmp al, 8           ;check if backspace key hit
-    je remove_char2
-    inc cx              ;increment array size by 1
-    mov [di], al
-    inc di
-    jmp input_char2
-    remove_char2:
-    cmp cx, 0
-    je setPos_ret2
-    dec cx              ;decrement array size by 1
-    dec di
-    mov [di], 00h
-    mov dl, 32          ;for removing char
-    mov ah, 2           ;
-    int 21h             ;
-    mov dl, 8           ;
-    mov ah, 2           ;
-    int 21h             ;
-    jmp input_char2
-    setPos_ret2:
-    goto_pos 22, 29
-    jmp input_char2
-    return2:            ;clear the screen and return procedure
-    clrScrn
-    call upper_bar 
-    goto_pos 2, 0           ;set cursor position beneath upper bar
-    mov ah, 0x3d             ;to open files
-    mov al, 00               ;file handler for reading files
-    mov dx, offset docName
-    int 21h
-    mov HANDLE, ax           ;setting up handler
-    mov ah, 0x3f             ;function for reading files
-    mov bx, HANDLE
-    mov cx, 1760             ;how many bytes to write
-    mov dx, offset matrix    ;where to save read data
-    int 21h       
-    mov dx, offset matrix    ;print the text on editor canvas
-    mov ah, 9                ;
-    int 21h                  ;
-    jmp MAIN_LOOP            
-           
-    UP:
-    cmp row, 2
-    je MAIN_LOOP 
-    dec curr_line
+    
+    
+UP:
+    cmp row, 0
+    je MAIN_LOOP
+    mov bx,0
+    mov bl,row
+    mov cl,col
+    cmp cl,numbers[bx]
+    jne up2
+    dec bl
+    mov cl,numbers[bx]
+    mov col,cl
+up2:
     dec row
-    goto_pos row, column
+    call far ptr goto_pos 
+    
     jmp MAIN_LOOP
-         
-    DOWN:
-    inc curr_line
-    inc row
-    goto_pos row, column 
-    jmp MAIN_LOOP
-           
-    LEFT:
-    dec column
-    goto_pos row, column
+	
+DOWN:
+	mov dl,lines
+	cmp row,dl
+	je MAIN_LOOP
+	mov bx,0
+    mov bl,row
+    mov cl,col
+    cmp cl,numbers[bx]
+jne down2
+    inc bl
+    mov cl,numbers[bx]
+    mov col,cl
+down2:
+	inc row
+	call far ptr  goto_pos 
+	jmp MAIN_LOOP
+	
+LEFT:
+    cmp col,0
+    je abnormall
+    dec col
+    call far ptr goto_pos 
+abnormall:
     jmp MAIN_LOOP
     
-    RIGHT:
-    inc column
-    goto_pos row, column
+RIGHT:
+	mov bh,0
+	mov bl,row
+	mov cl,col
+    cmp cl,numbers[bx]
+    je abnormalr
+    inc col
+    call far ptr goto_pos
+abnormalr:
     jmp MAIN_LOOP
     
-    ENTER:      
-    newline         ;newline macro
-    mov [si], 10    ;move newline into array 
-    inc si
-    mov dl, curr_char
-    mov [di], dl
-    inc di
-    inc curr_line
-    mov curr_char, 0
-    inc row             ;increment row number
-    mov column, 0       ;get hold of 0th Col for Navigation
-    goto_pos row, 0     ;to get on newline 0th column
+delchar:
+	mov dh,row
+	mov dl,col
+	cmp col,0
+	je delline
+	mov bx,0
+	mov bl,row
+	Video_addr 80,0
+	mov cl,col
+movfore:
+	;mov al,es:[di+2]
+	;mov es:[di],al
+	;add di,2
+	;mov al,es:[di]
+	;cmp al,0
+	;je next2
+	mov al,es:[di]
+	mov es:[di-2],al
+	add di,2
+	inc cl
+	cmp cl,numbers[bx]
+	ja next2
+jmp movfore
+next2:
+	dec deta
+	dec numbers[bx]
+	dec col
+	call far ptr goto_pos
+	jmp MAIN_LOOP
+delline:
+	mov dl,lines
+	mov bl,row
+	sub dl,bl
+	push dx
+	jz next6
+	mov bh,0
+	mov ah,numbers[bx]
+	mov cl,8
+delnumbers:
+	mov al,numbers[bx+1]
+	shl ax,cl
+	mov numbers[bx],ah
+	inc bl
+	dec dl
+	jz next6
+jmp delnumbers
+next6:
+
+	;删除屏幕内容
+	pop ax
+	cmp al,0
+	;jz next7
+	mov dh,row
+	mov dl,0
+delscr:
+	Video_addr 80,0
+	push di
+	inc dh
+	Video_addr 80,0
+	mov si,di
+	pop di
+	mov cx,79d
+copyline3:
+	mov ah,es:[si]
+	mov es:[di],ah
+	add si,2
+	add di,2
+loop copyline3
+	dec al
+	cmp al,0ffh
+	je next7
+jmp delscr
+
+next7:
+	dec lines
+	dec row
+	call far ptr goto_pos
     jmp MAIN_LOOP
     
-    BACKSPACE:
-    ;IF TRUE
-    cmp curr_line, 2    ;see if cursor is on the very 1st line of document
-    ;THEN DO THIS
-    je rmv              ;if TRUE, then just Remove the chars from matrix
-    ;IF TRUE
-    cmp curr_char, 0    ;see if cursor is on the 0th POS on most left
-    ;THEN DO THIS
-    je goBackLine       ;if TRUE, then go back to upper row at the latest character's POS
-    ;ELSE DO THIS
-    remove
-    dec curr_char
-    dec column
-    dec si
-    mov [si], 00h
-    jmp MAIN_LOOP
-    rmv:
-    remove
-    dec curr_char
-    dec column
-    dec si              ;decrement si
-    mov [si], 00h       ;fill NULL in removed char space in array 
-    jmp MAIN_LOOP
-    goBackLine:
-    dec curr_line
-    dec row
-    dec di
-    mov dl, [di]
-    mov column, dl
-    goto_pos curr_line, dl  ;go to the last character position in previous row
-    mov dl, [di]        ;moving in another register because size doesn't match
-    mov curr_char, dl   ;to reset the cursor to the last position of previous line
-    jmp MAIN_LOOP
-        
-MAIN ENDP
-END MAIN 
+toSAVE:
+	call far ptr SAVE
+	jmp MAIN_LOOP
+    
+   
+    jmp exit
+error:
+	mov ah,09h
+	lea dx,errorinfo
+	int 21h
+exit:
+    MOV AH,4CH
+    INT 21H
+main endp
+CODES ENDS
+
+code2 segment
+	assume cs:code2,ds:DATAS
+clearscreen proc far
+	push ax
+	push bx
+	push cx
+	push dx
+	
+	mov ah,6
+	mov al,0
+	mov bh,7
+	mov ch,0
+	mov cl,0
+	mov dh,24
+	mov dl,79
+	int 10h
+	
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	
+	retf
+clearscreen endp
+
+goto_pos proc far
+	push ax
+	push dx
+	mov ah,02h
+	mov dh,row
+	mov dl,col
+	int 10h
+	pop dx
+	pop ax
+	retf
+goto_pos endp
+
+SAVE proc far
+	;将指针移动至文件首
+	mov cx,0
+	mov row,cl
+	mov col,cl
+	mov dx,cx
+	mov al,dl
+	mov bx,fh
+	mov ah,42h
+	int 21h
+	
+	mov si,0
+	mov di,0
+	mov ax,0b800h
+	mov es,ax
+	mov bx,0
+tobuffer:
+	
+	;cmp dl,0
+	;je toCR
+	mov cl,col
+	cmp cl,numbers[bx]
+	je toCR
+	mov dl,es:[si]
+	mov buffer[di],dl
+	inc col
+	inc di
+	add si,2
+	jmp tobuffer
+toCR:
+	dec lines
+	cmp lines,0ffh
+	je endwback
+	inc bl
+	xchg di,si
+	inc row
+	mov dh,row
+	mov dl,0
+	mov col,dl
+	Video_addr 80,0
+	xchg di,si
+	
+	;缓冲区写入回车CRLF
+	mov dl,13d
+	mov buffer[di],dl
+	inc di
+	mov dl,10d
+	mov buffer[di],dl
+	inc di
+	
+	;mov cl,lines
+	;cmp cl,0
+	;je next4
+jmp tobuffer
+;将最后一行写回缓冲区
+;next4:
+;	mov dl,es:[si]
+;	cmp dl,32d
+;	je endwback
+;	mov buffer[di],dl
+;	inc di
+;	add si,2
+;jmp next4
+endwback:
+	;inc di
+	;mov cx,' '
+	;mov buffer[di],cl
+	mov dl,0
+	mov buffer[di],dl
+	
+	mov cx,di
+	;mov al,2
+	;mov ah,42h
+	;mov cx,0
+	;mov dx,0
+	;int 21h
+	
+	
+	mov dx,deta
+	cmp dx,0
+jg no
+	;mov cx,0ffffh
+	;int 21h
+	;mov ah,40h
+	;mov cx,0
+	;int 21h
+	lea dx,filename[2]
+    mov ah,3ch
+    mov cx,0
+    int 21h
+    
+jnc nextx
+	push dx
+	lea dx,errorx
+	mov ah,09h
+	int 21h
+	pop dx
+nextx:
+
+    mov ah,3dh
+    mov al,2
+    int 21h
+    mov fh,ax
+    
+jnc nextd
+	push dx
+	lea dx,errord
+	mov ah,09h
+	int 21h
+	pop dx
+nextd:
+
+no:
+	mov cx,di
+	mov bx,fh
+	lea dx,buffer
+	mov ah,40h
+	int 21h
+	
+	mov ah,3eh
+	int 21h
+	retf
+SAVE endp
+code2 ends
+
+    END START
